b6a4ed41ce3235d06c034ca413e84a9dd353d81d
mamoanwar97 <mamoanwar97@gmail.com>
2019-10-18 16:43:59 +0200

    pc&instructioned&pcadder separated + file for testing together

--- PC.v
+++ PC.v
@@ -1,1 +1,6 @@
-
+module PC(input_pc , output1);
+input wire[12:0] input_pc;
+output reg[12:0] output1;
+always@(input_pc)
+begin output1 <= input_pc; end
+endmodule
--- pc_pcAdder_InsMem.v
+++ pc_pcAdder_InsMem.v
@@ -1,3 +1,13 @@
+module plus_f_adder(pcPlus,pc,clk);
+input wire [12:0] pc;
+output reg[12:0] pcPlus;
+input wire clk;
+always@(posedge clk) begin
+pcPlus = pc +1; 
+$display ("PC=%d,pcPlus=%d",pc,pcPlus);
+end
+endmodule
+
 module Instruction_memory(instruction,clk,pc);
 output reg[31:0] instruction;
 input [12:0] pc;
@@ -25,6 +35,13 @@ begin
 instruction <= Imem[pc]; 
 $display ("instruction: %d",Imem[pc]);
 end
+endmodule
+
+module PC(input_pc , output1);
+input wire[12:0] input_pc;
+output reg[12:0] output1;
+always@(input_pc)
+begin output1 <= input_pc; end
 endmodule
 
 module tb_initialize_Imem();
